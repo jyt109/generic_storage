1c0e94a878ab32a48d496b9f5aae6a1569d498a3
whatdacollege <jeffreytang@jeffreys-mbp-2.lan>
2019-11-19 13:13:25 -0800

    v 0.1

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from ._google_drive_storage import GoogleDriveStorage  # noqa
+from ._storage import Storage  # noqa
---
+++ tests/test_google_drive_storage.py
@@ -0,0 +1,91 @@
+import os
+
+import pytest
+
+from .._google_drive_storage import GoogleDriveStorage
+
+
+@pytest.fixture(scope="module")
+def google_drive() -> GoogleDriveStorage:
+    environ = os.environ
+    settings_env = "GDRIVE_SETTING_PATH"
+    cred_env = "GDRIVE_CREDNENTIAL_PATH"
+    if settings_env not in environ or cred_env not in environ:
+        raise Exception(f"Set {settings_env} and {cred_env} env variables")
+    settings_path = os.environ[settings_env]
+    cred_path = os.environ[cred_env]
+    return GoogleDriveStorage(
+        setting_file_name=settings_path, credential_file_name=cred_path
+    )
+
+
+def test_data_path() -> str:
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+
+
+def test_create_dir(google_drive: GoogleDriveStorage) -> None:
+    google_drive.create_file("test")
+    google_drive.create_file("test/sub_dir_1")
+    google_drive.create_file("test/sub_dir_2")
+    assert google_drive.path_exists("test") is not None
+    assert google_drive.path_exists("test/sub_dir_1") is not None
+    assert google_drive.path_exists("test/sub_dir_2") is not None
+
+
+def test_create_zip_file(google_drive: GoogleDriveStorage) -> None:
+    google_drive.create_file(
+        remote_path="test/sub_dir_1/test.zip",
+        local_path=os.path.join(test_data_path(), "test.zip"),
+    )
+    assert google_drive.path_exists("test/sub_dir_1/test.zip") is not None
+
+
+def test_create_json_file(google_drive: GoogleDriveStorage) -> None:
+    google_drive.create_file(
+        remote_path="test/sub_dir_2/test.json",
+        local_path=os.path.join(test_data_path(), "test.json"),
+    )
+    assert google_drive.path_exists("test/sub_dir_2/test.json") is not None
+
+
+def test_create_txt_file(google_drive: GoogleDriveStorage) -> None:
+    google_drive.create_file(
+        remote_path="test/sub_dir_1/test.txt",
+        local_path=os.path.join(test_data_path(), "test.txt"),
+    )
+    assert google_drive.path_exists("test/sub_dir_1/test.txt") is not None
+
+
+def test_list_dir(google_drive: GoogleDriveStorage) -> None:
+    sub_dir_1_files = google_drive.list_files("test/sub_dir_1")
+    sub_dir_2_files = google_drive.list_files("test/sub_dir_2")
+    assert set(sub_dir_1_files) == {"test.zip", "test.txt"}
+    assert set(sub_dir_2_files) == {"test.json"}
+
+
+def test_download_files(google_drive: GoogleDriveStorage) -> None:
+    local_write_path = os.path.join(test_data_path(), "test_download.txt")
+    google_drive.download_file(
+        remote_path="test/sub_dir_1/test.txt", local_path=local_write_path
+    )
+    with open(local_write_path) as f:
+        assert f.read().strip() == "test"
+    os.remove(local_write_path)
+
+
+def test_delete_files(google_drive: GoogleDriveStorage) -> None:
+    google_drive.delete_file("test/sub_dir_1/test.txt")
+    assert google_drive.path_exists("test/sub_dir_1/test.txt") is None
+    assert google_drive.path_exists("test/sub_dir_1/test.zip") is not None
+    assert google_drive.path_exists("test/sub_dir_2/test.json") is not None
+
+    google_drive.delete_file("test/sub_dir_1")
+    assert google_drive.path_exists("test/sub_dir_1/test.txt") is None
+    assert google_drive.path_exists("test/sub_dir_1/test.zip") is None
+    assert google_drive.path_exists("test/sub_dir_2/test.json") is not None
+
+    google_drive.delete_file("test")
+    assert google_drive.path_exists("test") is None
+    assert google_drive.path_exists("test/sub_dir_1/test.txt") is None
+    assert google_drive.path_exists("test/sub_dir_1/test.zip") is None
+    assert google_drive.path_exists("test/sub_dir_2/test.json") is None
